5595af03db147453d279dcaf138761e0ef2c0095
Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2022-08-28 23:38:34 -0400

    Update fitness_app_st.py

--- fitness_app_st.py
+++ fitness_app_st.py
@@ -11,7 +11,7 @@ pd.options.mode.chained_assignment = None
 #endregion
 
 #region Read
-df = pd.read_excel('/fitness_data.xlsx', engine='openpyxl')
+df = pd.read_excel('fitness_data.xlsx', engine='openpyxl')
 #endregion
 
 #region Preprocessing
