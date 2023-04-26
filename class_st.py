9272aadbb75a1b4ce0e629006394270ebc1cffdf
Daniel Aldana <81043065+aldanajd@users.noreply.github.com>
2023-04-25 22:27:29 -0400

    Update class_st.py

--- class_st.py
+++ class_st.py
@@ -1,60 +1,393 @@
-import subprocess
-
-# Install tensorflow package
-subprocess.check_call(["pip", "install", "tensorflow"])
-
+#region Dependencies
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
 import streamlit as st
-import numpy as np
-import tensorflow as tf
-from tensorflow.keras.models import load_model
-from PIL import Image
 
-#Load the pre-trained model
-classifier_model = load_model('/app/clothing_classification/Model')
+from datetime import datetime
+from plotly.subplots import make_subplots
 
-#Define the categories to match the output of the pre-trained model
-classifier_dictionary = {
-    0:'Glasses',
-    1:'Jeans',
-    2:'Shoes'
+pd.options.mode.chained_assignment = None
+#endregion
+
+#region Read
+df = pd.read_csv('fitness_data.csv')
+#endregion
+
+#region Preprocessing
+##Fill nulls with previous value
+df.fillna(method='ffill', inplace=True)
+
+##Convert Date
+df['date'] = pd.to_datetime(df['date'])
+
+##Today's Date
+date_now = datetime.now().date()
+year_now = date_now.year
+month_now = date_now.month
+
+##Take data from today's date
+df = df[df['date'] <= pd.to_datetime(date_now)]
+
+##Calculate self_difference
+def self_difference(df):
+
+    columns = df.iloc[:,2:].columns
+
+    for column in columns:
+        new_column = column + '_%diff'
+        new_column_2 = new_column + '_start'
+        df[new_column] = round((df[column] * 100) / df[column].shift() - 100,1)
+        df[new_column_2] = round((df[column] * 100) / df[column][0] - 100,1)
+
+    return df
+
+df = self_difference(df)
+#endregion 
+
+#region Sidebar (Filters)
+month_map = {
+    'January': 1,
+    'February': 2,
+    'March': 3,
+    'April': 4,
+    'May': 5,
+    'June': 6,
+    'July': 7,
+    'August': 8,
+    'September': 9,
+    'October': 10,
+    'November': 11,
+    'December': 12
 }
 
-#Function to pre-process the input image to match the expected input of the pre-trained model, then predict the class
-def prediction(img):
-    """
-    Preprocess the single image to match the dimensions of the expected input, and the predicts the class label
-    Args:
-        img: Uploaded single image
-    """
-    img = Image.open(img)
-    img = img.resize((90, 120))
-    img = np.asarray(img)
-    img = np.expand_dims(img, axis=0)
-    indx = classifier_model.predict(img, verbose=0).argmax()
+month_map1 = {
+    1 : 'January',
+    2 : 'February',
+    3 : 'March',
+    4 : 'April',
+    5 : 'May',
+    6 : 'June',
+    7 : 'July',
+    8 : 'August',
+    9 : 'September',
+    10 : 'October',
+    11 : 'November',
+    12 : 'December'
+}
 
-    return classifier_dictionary[indx]
+unique_years = ['All']
+months = ['All']
 
-# Set page title
-st.set_page_config(page_title="Image Classifier")
+unique_years.extend(list(set(df['date'].dt.year)))
+unique_months = list(set(df['date'].dt.month))
 
-# Add title to app
-st.title("Image Classifier App!")
+months.extend([month_map1.get(n) for n in unique_months])
 
-# Define function to upload image
-def upload_image():
-    """
-    Sets a button to upload the image then apply the prediction function to it
-    """
-    #Button
-    uploaded_file = st.file_uploader("Choose an image file of Glasses, Jeans or Shoes", type=["jpg", "jpeg", "png"])
-    if uploaded_file is not None:
-        #Preprocess, Predict
-        classifier_label = prediction(uploaded_file)
-        st.markdown(f"<h3 style='text-align: center;'>{classifier_label}!</h3>", unsafe_allow_html=True)
-        #Display the uploaded image
-        st.image(uploaded_file, caption=f"{classifier_label}", use_column_width=True)
-    else:
-        st.warning("Please upload an image file of Glasses, Jeans or Shoes.")
+year = st.sidebar.selectbox('Select Year', options=unique_years, index=unique_years.index(year_now))
+month = st.sidebar.selectbox('Select Month', options=months, index=unique_months.index(month_now)+1)
 
-# Display upload widget
-upload_image()
+if year == 'All':
+    df1 = df
+else:
+    df1 = df[df['date'].dt.year == year]
+
+if month == 'All':
+    pass
+else:
+    df1 = df1[df1['date'].dt.month == month_map.get(month)]
+#endregion
+
+#region Tabs
+tab1, tab2, tab3 = st.tabs(["Timeline", "Progress since the Beginning", "Progress since Previous Day"])
+
+with tab1:
+    x = df1['date']
+
+    y1 = df1['johanna_weight']
+    y2 = df1['adam_weight']
+    y3 = df1['laura_weight']
+    y4 = df1['michael_weight']
+
+    y_1 = df1['johanna_fat']
+    y_2 = df1['adam_fat']
+    y_3 = df1['laura_fat']
+    y_4 = df1['michael_fat']
+
+    fig = make_subplots(rows=2, cols=1,
+                        shared_xaxes=True,
+                        vertical_spacing=0.07,
+                        subplot_titles=['Weight(Kg) - Timeline', 
+                        'Fat(%) - Timeline'])
+
+    ##Weight
+    fig.add_trace(go.Scatter(x=x, y=y1, name='Johanna', line={'color':'orange'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y2, name='Adam', line={'color':'red'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y3, name='Laura', line={'color':'aqua'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y4, name='Michael', line={'color':'green'}),
+                row=1, col=1)
+
+    ##Fat
+    fig.add_trace(go.Scatter(x=x, y=y_1, name='Johanna', line={'color':'orange'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_2, name='Adam', line={'color':'red'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_3, name='Laura', line={'color':'aqua'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_4, name='Michael', line={'color':'green'}, showlegend=False),
+                row=2, col=1)
+
+    fig.update_layout(height=500, width=800, hovermode='x unified')
+
+    ##Graph
+    st.plotly_chart(fig, use_container_width=True)
+
+with tab2:
+    x = df1['date']
+
+    y1 = df1['johanna_weight_%diff_start']
+    y2 = df1['adam_weight_%diff_start']
+    y3 = df1['laura_weight_%diff_start']
+    y4 = df1['michael_weight_%diff_start']
+
+    y_1 = df1['johanna_fat_%diff_start']
+    y_2 = df1['adam_fat_%diff_start']
+    y_3 = df1['laura_fat_%diff_start']
+    y_4 = df1['michael_fat_%diff_start']
+
+    fig = make_subplots(rows=2, cols=1,
+                        shared_xaxes=True,
+                        vertical_spacing=0.07,
+                        subplot_titles=['Weight(%) - Progress since the Beginning', 
+                        'Fat(%) - Progress since the Beginning'])
+
+    ##Weight
+    fig.add_trace(go.Scatter(x=x, y=y1, name='Johanna', line={'color':'orange'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y2, name='Adam', line={'color':'red'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y3, name='Laura', line={'color':'aqua'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y4, name='Michael', line={'color':'green'}),
+                row=1, col=1)
+
+    ##Fat
+    fig.add_trace(go.Scatter(x=x, y=y_1, name='Johanna', line={'color':'orange'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_2, name='Adam', line={'color':'red'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_3, name='Laura', line={'color':'aqua'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_4, name='Michael', line={'color':'green'}, showlegend=False),
+                row=2, col=1)
+
+    fig.update_layout(height=500, width=800, hovermode='x unified')
+
+    ##Graph
+    st.plotly_chart(fig, use_container_width=True)
+
+with tab3:
+    x = df1['date']
+
+    y1 = df1['johanna_weight_%diff']
+    y2 = df1['adam_weight_%diff']
+    y3 = df1['laura_weight_%diff']
+    y4 = df1['michael_weight_%diff']
+
+    y_1 = df1['johanna_fat_%diff']
+    y_2 = df1['adam_fat_%diff']
+    y_3 = df1['laura_fat_%diff']
+    y_4 = df1['michael_fat_%diff']
+
+    fig = make_subplots(rows=2, cols=1,
+                        shared_xaxes=True,
+                        vertical_spacing=0.07,
+                        subplot_titles=['Weight(%) - Progress since Previous Day', 
+                        'Fat(%) - Progress since Previous Day'])
+
+    ##Weight
+    fig.add_trace(go.Scatter(x=x, y=y1, name='Johanna', line={'color':'orange'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y2, name='Adam', line={'color':'red'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y3, name='Laura', line={'color':'aqua'}),
+                row=1, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y4, name='Michael', line={'color':'green'}),
+                row=1, col=1)
+
+    ##Fat
+    fig.add_trace(go.Scatter(x=x, y=y_1, name='Johanna', line={'color':'orange'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_2, name='Adam', line={'color':'red'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_3, name='Laura', line={'color':'aqua'}, showlegend=False),
+                row=2, col=1)
+
+    fig.add_trace(go.Scatter(x=x, y=y_4, name='Michael', line={'color':'green'}, showlegend=False),
+                row=2, col=1)
+
+    fig.update_layout(height=500, width=800, hovermode='x unified')
+
+    ##Graph
+    st.plotly_chart(fig, use_container_width=True)
+#endregion
+
+#region Graphs
+##Weight data
+x = df1['date']
+
+y1 = df1['johanna_weight']
+y2 = df1['adam_weight']
+y3 = df1['laura_weight']
+y4 = df1['michael_weight']
+
+Y1 = df1['johanna_weight_%diff_start']
+Y2 = df1['adam_weight_%diff_start']
+Y3 = df1['laura_weight_%diff_start']
+Y4 = df1['michael_weight_%diff_start']
+
+y_1 = df1['johanna_weight_%diff']
+y_2 = df1['adam_weight_%diff']
+y_3 = df1['laura_weight_%diff']
+y_4 = df1['michael_weight_%diff']
+
+fig = make_subplots(rows=3, cols=1,
+                    shared_xaxes=True,
+                    vertical_spacing=0.05,
+                    subplot_titles=['Weight(Kg) - Timeline', 
+                    'Weight(%) - Progress since the Beginning',
+                    'Weight(%) - Progress since Previous Day'])
+
+##Weight
+fig.add_trace(go.Scatter(x=x, y=y1, name='Johanna', line={'color':'orange'}),
+            row=1, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y2, name='Adam', line={'color':'red'}),
+            row=1, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y3, name='Laura', line={'color':'aqua'}),
+            row=1, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y4, name='Michael', line={'color':'green'}),
+            row=1, col=1)
+
+##Weight difference from Start
+fig.add_trace(go.Scatter(x=x, y=Y1, name='Johanna', line={'color':'orange'}, showlegend=False),
+            row=2, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=Y2, name='Adam', line={'color':'red'}, showlegend=False),
+            row=2, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=Y3, name='Laura', line={'color':'aqua'}, showlegend=False),
+            row=2, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=Y4, name='Michael', line={'color':'green'}, showlegend=False),
+            row=2, col=1)
+
+##Weight difference cumulative
+fig.add_trace(go.Scatter(x=x, y=y_1, name='Johanna', line={'color':'orange'}, showlegend=False),
+            row=3, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y_2, name='Adam', line={'color':'red'}, showlegend=False),
+            row=3, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y_3, name='Laura', line={'color':'aqua'}, showlegend=False),
+            row=3, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y_4, name='Michael', line={'color':'green'}, showlegend=False),
+            row=3, col=1)              
+
+fig.update_layout(height=800, width=800, hovermode='x unified')
+
+##Graph
+st.plotly_chart(fig, use_container_width=True)
+
+##Fat data
+x = df1['date']
+
+y1 = df1['johanna_fat']
+y2 = df1['adam_fat']
+y3 = df1['laura_fat']
+y4 = df1['michael_fat']
+
+Y1 = df1['johanna_fat_%diff_start']
+Y2 = df1['adam_fat_%diff_start']
+Y3 = df1['laura_fat_%diff_start']
+Y4 = df1['michael_fat_%diff_start']
+
+y_1 = df1['johanna_fat_%diff']
+y_2 = df1['adam_fat_%diff']
+y_3 = df1['laura_fat_%diff']
+y_4 = df1['michael_fat_%diff']
+
+fig = make_subplots(rows=3, cols=1,
+                    shared_xaxes=True,
+                    vertical_spacing=0.05,
+                    subplot_titles=['Fat(%) - Timeline', 
+                    'Fat(%) - Progress since the Beginning',
+                    'Fat(%) - Progress since Previous Day'])
+
+##Fat
+fig.add_trace(go.Scatter(x=x, y=y1, name='Johanna', line={'color':'orange'}),
+            row=1, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y2, name='Adam', line={'color':'red'}),
+            row=1, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y3, name='Laura', line={'color':'aqua'}),
+            row=1, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y4, name='Michael', line={'color':'green'}),
+            row=1, col=1)
+
+##Fat difference from Start
+fig.add_trace(go.Scatter(x=x, y=Y1, name='Johanna', line={'color':'orange'}, showlegend=False),
+            row=2, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=Y2, name='Adam', line={'color':'red'}, showlegend=False),
+            row=2, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=Y3, name='Laura', line={'color':'aqua'}, showlegend=False),
+            row=2, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=Y4, name='Michael', line={'color':'green'}, showlegend=False),
+            row=2, col=1)
+
+##Fat difference cumulative
+fig.add_trace(go.Scatter(x=x, y=y_1, name='Johanna', line={'color':'orange'}, showlegend=False),
+            row=3, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y_2, name='Adam', line={'color':'red'}, showlegend=False),
+            row=3, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y_3, name='Laura', line={'color':'aqua'}, showlegend=False),
+            row=3, col=1)
+
+fig.add_trace(go.Scatter(x=x, y=y_4, name='Michael', line={'color':'green'}, showlegend=False),
+            row=3, col=1)              
+
+fig.update_layout(height=800, width=800, hovermode='x unified')
+
+##Graph
+st.plotly_chart(fig, use_container_width=True)
+#endregion
